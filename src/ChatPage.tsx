00b588299c3d86ff087a545ddefe5b550640f22e
christopher.hogg <christopher.hogg@blik.io>
2019-07-12 09:28:24 +0200

    feat: basic setup with chat page.

---
+++ src/ChatPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const ChatPage: React.FC = () => {
+  return (
+      <React.Fragment> 
+      <h3> Chat Page </h3>
+      </React.Fragment>
+  );
+}
+
+export default ChatPage;
