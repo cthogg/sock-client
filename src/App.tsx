c39c35f4388acd69a024d9680c6618d85778fc75
christopher.hogg <christopher.hogg@blik.io>
2019-07-12 10:54:56 +0200

    feat: change the message on submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
-import ChatPage from './ChatPage';
+import React from "react";
+import "./App.css";
+import ChatPage from "./ChatPage";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:4000");
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <ChatPage></ChatPage>
+      <ChatPage />
     </div>
   );
-}
+};
 
 export default App;
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,35 +1,40 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Formik, Field, Form, FormikActions } from "formik";
 
 interface Values {
   firstName: string;
 }
 
-const ChatForm: React.SFC<{}> = () => (
-  <div className="container">
-    <Formik
-      initialValues={{
-        firstName: ""
-      }}
-      onSubmit={(values: Values, { setSubmitting }: FormikActions<Values>) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 10);
-      }}
-      render={() => (
-        <Form>
-          <Field
-            id="firstName"
-            name="firstName"
-            placeholder="Message"
-            type="text"
-          />
-          <button type="submit">Submit</button>
-        </Form>
-      )}
-    />
-  </div>
-);
+const ChatForm: React.SFC<{}> = () => {
+  const [message, setMessage] = useState("");
+
+  return (
+    <div className="container">
+      <Formik
+        initialValues={{
+          firstName: ""
+        }}
+        onSubmit={(
+          values: Values,
+          { setSubmitting }: FormikActions<Values>
+        ) => {
+          setMessage(JSON.stringify(values, null, 2));
+        }}
+        render={() => (
+          <Form>
+            <Field
+              id="firstName"
+              name="firstName"
+              placeholder="Message"
+              type="text"
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      />
+      <p> {message} </p>
+    </div>
+  );
+};
 
 export default ChatForm;
