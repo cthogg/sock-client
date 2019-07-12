ad60864ea8ae5bcd47e3063b25b1685cfaba2c48
christopher.hogg <christopher.hogg@blik.io>
2019-07-12 11:20:13 +0200

    feat:  add text

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "./App.css";
 import ChatPage from "./ChatPage";
-import io from "socket.io-client";
-
-const socket = io("http://localhost:4000");
 
 const App: React.FC = () => {
   return (
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,34 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Formik, Field, Form, FormikActions } from "formik";
+import io from "socket.io-client";
+
+const CHAT_MESSAGE = "chat message";
+
+const socket = io("http://localhost:4000");
 
 interface Values {
-  firstName: string;
+  messageText: string;
 }
 
 const ChatForm: React.SFC<{}> = () => {
   const [message, setMessage] = useState("");
+  const [placeholderText, setplaceholderText] = useState("last message");
+
+  useEffect(() => {
+    socket.on(CHAT_MESSAGE, (payload: string) => {
+      const messageText = JSON.parse(payload).messageText;
+      setMessage(messageText);
+      setplaceholderText(messageText);
+    });
+  });
 
   return (
     <div className="container">
       <Formik
         initialValues={{
-          firstName: ""
+          messageText: ""
         }}
         onSubmit={(
           values: Values,
           { setSubmitting }: FormikActions<Values>
         ) => {
-          setMessage(JSON.stringify(values, null, 2));
+          const message = JSON.stringify(values, null, 2);
+          setMessage(values.messageText);
+          socket.emit(CHAT_MESSAGE, message);
         }}
         render={() => (
           <Form>
             <Field
-              id="firstName"
-              name="firstName"
-              placeholder="Message"
+              id="messageText"
+              name="messageText"
+              placeholder={placeholderText}
               type="text"
             />
-            <button type="submit">Submit</button>
+            <button type="submit">Edit Text</button>
           </Form>
         )}
       />
