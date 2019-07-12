da37baadcfe35e8eb09a4008edb30cf99d88def1
christopher.hogg <christopher.hogg@blik.io>
2019-07-12 12:28:06 +0200

    Updates

---
+++ precache-manifest.850f8d36ba58fa85b6d89efc608c5c64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d955509d4caf35f7b7f9e59d49ca6d4",
+    "url": "/sock-client/index.html"
+  },
+  {
+    "revision": "a05a6acd04ce00d1dff9",
+    "url": "/sock-client/static/css/main.bdcf178b.chunk.css"
+  },
+  {
+    "revision": "08c62f6980c604ec96e2",
+    "url": "/sock-client/static/js/2.e7115102.chunk.js"
+  },
+  {
+    "revision": "a05a6acd04ce00d1dff9",
+    "url": "/sock-client/static/js/main.479929f8.chunk.js"
+  },
+  {
+    "revision": "0754d652b1f9947c63bc",
+    "url": "/sock-client/static/js/runtime~main.73c2bc57.js"
+  }
+]);
